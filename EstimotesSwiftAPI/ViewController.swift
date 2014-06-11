4584106291b8e660e760caee3eb8e90fb511a82e
Paulo GF <paulinhog84@gmail.com>
2014-06-11 16:55:52 -0500

    Initial Commit

---
+++ EstimotesSwiftAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EstimotesSwiftAPI
+//
+//  Created by PauloGF on 6/11/14.
+//  Copyright (c) 2014 LTG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
